549bd2aad4b885efcfa55ca569ac1d40d5fb6463
sserges <agounkeserge@gmail.com>
2020-10-19 16:05:41 +0000

    Send a Simple Response

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
+const products = require('./data/products');
+
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello World</h1>');
-    res.end();
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    res.end(JSON.stringify(products));
 });
 
 

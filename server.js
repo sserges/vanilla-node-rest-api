90654b1cfc37f490cd5198d3dd166d37f8e10486
sserges <agounkeserge@gmail.com>
2020-10-19 16:14:03 +0000

    Simple Routing

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const products = require('./data/products');
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify(products));
+    if (req.url === '/api/products' && req.method === 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(products));
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify({ message: 'Route Not Found' }));
+    }
 });
 
 

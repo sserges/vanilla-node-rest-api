3357298bab3abae2db855dd42a4f9b7e8f886f6b
sserges <agounkeserge@gmail.com>
2020-10-20 01:02:12 +0000

    Get All Products

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const http = require('http');
 
-const products = require('./data/products');
+const { getProducts } = require('./controllers/productController');
 
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
     if (req.url === '/api/products' && req.method === 'GET') {
-        res.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-        res.end(JSON.stringify(products));
+        getProducts(req, res);
     } else {
         res.writeHead(404, {
             'Content-Type': 'application/json'

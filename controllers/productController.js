ebafc8e926a9bb361a97fc7e255b4fc0dcf6f408
sserges <agounkeserge@gmail.com>
2020-10-20 16:18:11 +0000

    Create Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,7 +36,28 @@ async function getProduct(req, res, id) {
     }
 }
 
+// @desc  Create a Product
+// @route POST /api/products
+async function createProduct(req, res) {
+    try {
+        const product = {
+            title: 'Test Product',
+            description: 'This is my product',
+            price: 100
+        };
+        const newProduct = await Product.create(product);
+
+        res.writeHead(201, {
+            'Content-Type': 'application/json'
+        });
+        return res.end(JSON.stringify(newProduct));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
     getProducts,
-    getProduct
+    getProduct,
+    createProduct
 }

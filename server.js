f0dd06f243285c246edfb3afdc6b9c4f74180c4e
sserges <agounkeserge@gmail.com>
2020-10-22 12:32:15 +0000

    README added

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
     if (req.url === '/api/products' && req.method === 'GET') {
         getProducts(req, res);
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
+    } else if (req.url.match(/\/api\/products\/([0-9a-z]+)/) && req.method === 'GET') {
         const id = req.url.split('/')[3];
-        console.log(req.url.match(/\/api\/products\/([0-9]+)/), req.method === 'GET');
+        console.log(req.url.match(/\/api\/products\/([0-9a-z]+)/), req.method === 'GET');
         getProduct(req, res, id);
     } else if ( req.url === '/api/products' && req.method === 'POST') {
         createProduct(req, res);
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+    } else if (req.url.match(/\/api\/products\/([0-9a-z]+)/) && req.method === 'PUT') {
         const id = req.url.split('/')[3];
         updateProduct(req, res, id);
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+    } else if (req.url.match(/\/api\/products\/([0-9a-z]+)/) && req.method === 'DELETE') {
         const id = req.url.split('/')[3];
         deleteProduct(req, res, id);
     } else {
         console.log(req.url, req.method);
-        console.log(req.url.match(/\/api\/products\/([0-9]+)/), req.method === 'DELETE');
+        console.log(req.url.match(/\/api\/products\/([0-9a-z]+)/), req.method === 'DELETE');
         res.writeHead(404, {
             'Content-Type': 'application/json'
         });
